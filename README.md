38f1a1786d5ffb8c3652ea8bca592e7e64240808
raistar <sitapradivyan01@gmail.com>
2024-05-05 12:09:15 +0530

    all done

---
+++ README.md
@@ -0,0 +1,21 @@
+
+ Scan QR using Quree
+
+This is a simple QR scanning app built in 📱 Android 
+
+
+## Badges
+
+Add badges from somewhere like: [shields.io](https://shields.io/)
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+
+
+
+## Authors
+
+- [@C4LLM3101](https://www.github.com/C4LLM3101)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ app/src/main/java/com/example/myquree/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.myquree;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         button = findViewById(R.id.scanner);
+         textView = findViewById(R.id.text);
+
+         button.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 IntentIntegrator intentIntegrator = new IntentIntegrator( MainActivity .this);
+                 intentIntegrator.setOrientationLocked(true);
+                 intentIntegrator.setPrompt("SCan a QRCode ");
+                 intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                 intentIntegrator.initiateScan();
+
+
+
+             }
+         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult intentResult =  IntentIntegrator .parseActivityResult(requestCode,resultCode,data);
+
+        if (intentResult!= null)
+        {
+            String contents = intentResult.getContents();
+            if (contents != null){
+                textView.setText(intentResult.getContents());
+
+            }
+        }    else {
+            super.onActivityResult(requestCode, resultCode, data);
+
+        }
+
+       /* super.onActivityResult(requestCode, resultCode, data);*/
+
+
+    }
+}
